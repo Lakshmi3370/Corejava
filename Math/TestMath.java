98a704f087dd5e467524a6f856751c6d2c665b91
Lakshmi3370 <lakshmidevi.xwokrkz@gmail.com>
2021-12-30 00:32:27 +0530

    Math

---
+++ Math/TestMath.java
@@ -0,0 +1,29 @@
+package com.xworkz.examples;
+
+import java.lang.Math;
+
+public class TestMath {
+
+	public static void main(String[] args) {
+
+			byte num1 = 24;    
+	        byte num2 = 4;  
+	        byte num3 = 27;
+	        
+	          
+	        // return the minimum of two numbers  
+	        System.out.println("Minimum Number: " +Math.min(num1,num2));   
+	        System.out.println("Maximun Number: " +Math.max(num1,num2));
+	        System.out.println("Sqrt of Number2: " +Math.sqrt(num2));
+	        System.out.println("Power of Number: " +Math.pow(num1,num2));
+	        System.out.println("Sin of Number: " +Math.sin(num2));
+	        System.out.println("cos of Number22: " +Math.cos(num2));
+	        System.out.println("Cube root of Number3: " +Math.cbrt(num3));
+
+	        System.out.println("PI Value: " +Math.PI);   
+
+		}
+		
+	}
+
+
